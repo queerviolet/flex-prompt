523545402043bb17539042ed73fd7c3d4bc6170c
Ashi Krishnan <i@ashi.io>
2023-12-05 15:33:52 -0800

    Track output_parser from Expect

--- src/flex_prompt/expect.py
+++ src/flex_prompt/expect.py
@@ -1,16 +1,20 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Callable
 
 from .context import Render, Part
 
 @dataclass(frozen=True, slots=True)
 class Expect:
   """Expect a response from the LLM."""
+
+  output_parser: Callable[[str], Any] = str
+
   def __call__(self, ctx: Render):
     print("call expect")
-    yield Expectation(ctx.tokens_remaining)
+    yield Expectation(ctx.tokens_remaining, self.output_parser)
 
 @dataclass(frozen=True, slots=True)
 class Expectation(Part):
   """An expected response"""
   token_count: int
+  output_parser: Callable[[str], Any]
