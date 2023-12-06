75b01c221d3d60844577a8f74264d3ab884f7d6c
Ashi Krishnan <i@ashi.io>
2023-12-05 18:20:54 -0800

    Cat join and mode options

--- src/flex_prompt/cat.py
+++ src/flex_prompt/cat.py
@@ -1,20 +1,32 @@
+from enum import StrEnum
 from typing import Any, Iterable
 from dataclasses import dataclass
 from .context import Render, Overflow
 
+Mode = StrEnum('Mode', ['block', 'clip'])
 @dataclass(init=False)
 class Cat:
   children: tuple[Iterable[Any], ...]
   flex_weight: int = 1
-  def __init__(self, *children: Iterable[Any], flex_weight=1):
+  def __init__(self, *children: Iterable[Any], flex_weight=1, join: Any='', mode: Mode = 'block'):
     self.children = children
     self.flex_weight = flex_weight
+    self.join = join
+    self.mode = mode
 
   def __call__(self, render: Render) -> Iterable:
+    rendered_join = render(self.join)
+    join_len = 0
+    first = True    
     for child in self.children:
       for item in child:
-        output = render(item)
-        if output.overflow_token_count > 0:
+        output = render(item, token_limit=render.tokens_remaining - join_len)
+        if self.mode == 'block' and output.overflow_token_count > 0:
           yield Overflow(output)
           return
+        if join_len: yield rendered_join
+        if first:
+          join_len = len(rendered_join)
+          first = False
         yield output
+        if output.overflow_token_count: return
---
+++ tests/test_cat.py
@@ -0,0 +1,37 @@
+from flex_prompt import Cat, render
+one = """
+and lo betide, the red sky opened upon us as though the crinkled
+hand of the heavens itself was reaching down.
+"""
+two = """
+we were witness to dark and terrible portents, whose nameless
+features we could not grasp with our mortal minds
+"""
+three = """
+it was only then, in the moment when cruel stars had long since
+wrung us dry, that the chinchillas arrived.
+"""
+
+def test_cat_block():
+  assert render(Cat([one, two, three]),
+                model='test-len-str',
+                token_limit=200).output == one
+
+def test_cat_clip():
+  rendering = render(Cat([one, two, three], mode='clip'),
+                     model='test-len-str',
+                     token_limit=200)
+  assert rendering.output == (one + two)[:200]
+  assert rendering.overflow_token_count == len(one) + len(two) - 200
+
+def test_cat_join_clip():
+  rendering = render(Cat([one, two, three], mode='clip', join='---'),
+                     model='test-len-str',
+                     token_limit=200)
+  assert rendering.output == (one + '---' + two)[:200]  
+
+def test_cat_join_block():
+  rendering = render(Cat([one, two, three], mode='block', join='---'),
+                     model='test-len-str',
+                     token_limit=len(one) + len(two) + int(0.5 * len(three)))                     
+  assert rendering.output == (one + '---' + two)
