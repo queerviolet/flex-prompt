f55d3ecf0978d04469649d44e5a364d1cce51f17
Ashi Krishnan <i@ashi.io>
2023-12-06 17:16:55 -0800

    Add flex_weight to Expect and remove output_parser for now (breaking change)

--- src/flex_prompt/expect.py
+++ src/flex_prompt/expect.py
@@ -6,17 +6,15 @@ from .context import Render, Part
 @dataclass(frozen=True, slots=True)
 class Expect:
   """Expect a response from the LLM."""
-
-  output_parser: Callable[[str], Any] = str
+  flex_weight: int = 1
 
   def __call__(self, ctx: Render):
-    yield Expectation(ctx.tokens_remaining, self.output_parser)
+    yield Expectation(ctx.tokens_remaining)
 
 @dataclass(frozen=True, slots=True)
 class Expectation(Part):
   """An expected response"""
   token_count: int
-  output_parser: Callable[[str], Any]
 
   @property
   def expected_token_count(self) -> int:
